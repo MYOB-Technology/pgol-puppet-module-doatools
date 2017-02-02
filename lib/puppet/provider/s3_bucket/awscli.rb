d53a28213c31bc6830187dad8c0ee19811d3a7e1
Michael Shaw <micptolshaw@intechnologywifi.com>
2017-02-02 16:20:16 +0000

    PAWS-30: Implemented the basic lifecycle methods for the s3_bucket component

--- lib/puppet/provider/s3_bucket/awscli.rb
+++ lib/puppet/provider/s3_bucket/awscli.rb
@@ -14,10 +14,81 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'json'
+require 'puppet_x/intechwifi/exceptions'
 require 'puppet_x/intechwifi/s3'
 
 Puppet::Type.type(:s3_bucket).provide(:awscli) do
   commands :awscli => "aws"
+
+  def create
+    args = [
+      's3api', 'create-bucket',
+      '--bucket', @resource[:name],
+      '--region', @resource[:region]
+    ]
+
+    awscli(args.flatten)
+
+    @property_hash[:name] = @resource[:name]
+    @property_hash[:region] = @resource[:region]
+
+  end
+
+  def destroy
+    args = [
+        's3api', 'delete-bucket',
+        '--bucket', @resource[:name],
+        '--region', @resource[:region]
+    ]
+
+    awscli(args.flatten)
+
+  end
+
+  def exists?
+    args = [
+        's3api', 'list-buckets'
+    ]
+
+    data = JSON.parse(awscli(args.flatten))
+    @account = data["Owner"]
+
+    raise PuppetX::IntechWIFI::Exceptions::NotFoundError, @resource[:name] if data["Buckets"].select{|x| x["Name"] == @resource[:name]}.length != 1
+
+
+    true
+  rescue PuppetX::IntechWIFI::Exceptions::NotFoundError
+    false
+  end
+
+  def flush
+    if @property_flush and @property_flush.length > 0
+    end
+  end
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  mk_resource_methods
+
+  def region=(value)
+    @property_flush[:region] = value
+  end
+
+  def grants=(value)
+    @property_flush[:grants] = value
+  end
+
+  def policy=(value)
+    @property_flush[:policy] = value
+  end
+
+  def cors=(value)
+    @property_flush[:cors] = value
+  end
+
 
 end
 
