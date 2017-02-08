305efeb756d897678ec13825f3903c3c8b5e47a5
Michael Shaw <micptolshaw@intechnologywifi.com>
2017-02-08 13:49:06 +0000

    PAWS-47: Implementation of the listeners property.  Handling of certificates for HTTPS connections is forced to use certificate ARN as there is no name property available that we can rely on to be unique

--- lib/puppet/provider/load_balancer/awscli.rb
+++ lib/puppet/provider/load_balancer/awscli.rb
@@ -73,7 +73,9 @@ Puppet::Type.type(:load_balancer).provide(:awscli) do
     data = search_results[:data][0]
     @arn = data["LoadBalancerArn"]
     @property_hash[:subnets] = data["AvailabilityZones"].map{|subnet| PuppetX::IntechWIFI::AwsCmds.find_name_by_id(@property_hash[:region], 'subnet', subnet["SubnetId"]){|*arg| awscli(*arg)} }
-
+    @property_hash[:listeners] = JSON.parse(awscli('elbv2', 'describe-listeners', '--region', @property_hash[:region], '--load-balancer-arn', @arn))["Listeners"].map do |x|
+      "#{x["Protocol"].downcase}://#{target_from_arn x["DefaultActions"][0]["TargetGroupArn"]}:#{x["Port"]}#{x["Certificates"].nil? ? "" : ("?certificate=" + x["Certificates"][0]["CertificateArn"])}"
+    end
     true
 
   rescue PuppetX::IntechWIFI::Exceptions::NotFoundError => e
@@ -88,7 +90,9 @@ Puppet::Type.type(:load_balancer).provide(:awscli) do
   def flush
     if @property_flush and @property_flush.length > 0
       set_subnets(@property_flush[:subnets]) if !@property_flush[:subnets].nil?
-
+      #@property_flush[:listeners].select{|x| !@property_hash[:listeners].include? x}.each{|x| create_listener(x)} if !@property_flush[:listeners].nil?
+      @property_flush[:listeners].select{|x| !@property_hash[:listeners].include?(x)}.each{|x| create_listener(x)} if !@property_flush[:listeners].nil?
+      @property_hash[:listeners].select{|x| !@property_flush[:listeners].include?(x)}.each{|x| destroy_listener(x)} if !@property_flush[:listeners].nil?
     end
   end
 
@@ -108,6 +112,60 @@ Puppet::Type.type(:load_balancer).provide(:awscli) do
 
   end
 
+  def create_listener(source)
+    match = /^(http[s]?):\/\/([a-z1-9\_]{3,255}):([0-9]{2,4})/.match(source)
+    proto = match[1]
+    target = match[2]
+    port = match[3]
+
+    match_cert = /^http[s]?:\/\/[a-z1-9\_]{3,255}:[0-9]{2,4}\?certificate=(.+)$/.match(source)
+    certificate = match_cert.nil? ? nil : match_cert[1]
+
+    args = [
+        'elbv2', 'create-listener',
+        '--region', @property_hash[:region],
+        '--load-balancer-arn', @arn,
+        '--protocol', proto.upcase,
+        '--port', port,
+        '--default-actions', "Type=forward,TargetGroupArn=#{find_target_by_name(@arn, target)}"
+    ]
+    args << ['--certificates', "CertificateArn=#{certificate}"] if proto=="https" and !certificate.nil?
+    awscli(args.flatten)
+
+  end
+
+  def destroy_listener(source)
+    match = /^http[s]?:\/\/([a-z1-9\_]{3,255}):[0-9]{2,4}/.match(source)
+    target = match[1]
+
+    listener_arn = JSON.parse(awscli('elbv2', 'describe-listeners', '--region', @property_hash[:region], '--load-balancer-arn', @arn))["Listeners"].select{ |x|
+      target_from_arn(x["DefaultActions"][0]["TargetGroupArn"]) == target
+    }.map{|x| x["ListenerArn"]}[0]
+
+    args = [
+        'elbv2', 'delete-listener',
+        '--region', @property_hash[:region],
+        '--listener-arn', listener_arn,
+    ]
+    awscli(args.flatten)
+
+  end
+
+  def target_from_arn(target_arn)
+    /^arn:aws:elasticloadbalancing:[a-z_\-0-9]+:[0-9]{12}:targetgroup\/([a-z_\-0-9]+)\/[0-9a-f]{16}$/.match(target_arn)[1]
+  end
+
+
+  def find_target_by_name(load_balancer_arn, name)
+    args = [
+        'elbv2', 'describe-target-groups',
+        '--region', @property_hash[:region],
+#        '--load-balancer-arn', load_balancer_arn,
+        '--names', name
+    ]
+    JSON.parse(awscli(args.flatten))["TargetGroups"][0]["TargetGroupArn"]
+  end
+
   mk_resource_methods
 
   def region=(value)
@@ -118,5 +176,8 @@ Puppet::Type.type(:load_balancer).provide(:awscli) do
     @property_flush[:subnets] = value
   end
 
+  def listeners=(value)
+    @property_flush[:listeners] = value
+  end
 
 end
