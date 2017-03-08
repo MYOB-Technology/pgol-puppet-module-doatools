ae220e3aec206de2c1ea44d05012ebb4ef15b685
Michael Shaw <m_shaw@Michaels-MacBook-Pro.local>
2017-03-08 16:56:20 +0000

    PAWS-71: created new files for egress_only_internet_gateway

---
+++ lib/puppet/provider/egress_only_internet_gateway/awscli.rb
@@ -0,0 +1,51 @@
+#  Copyright (C) 2017 IntechnologyWIFI / Michael Shaw
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'json'
+require 'puppet_x/intechwifi/constants'
+require 'puppet_x/intechwifi/logical'
+require 'puppet_x/intechwifi/awscmds'
+require 'puppet_x/intechwifi/exceptions'
+
+Puppet::Type.type(:egress_only_internet_gateway).provide(:awscli) do
+  commands :awscli => "aws"
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  def exists?
+
+    false
+  end
+
+  def flush
+
+  end
+
+  def initialize(value={})
+    super(value)
+    @property_flush = {}
+  end
+
+  mk_resource_methods
+
+
+end
+
