e0ef8bf8be3be7c6ccb057fd36ce2e532a04e086
Shantanu Padhye <Shantanu.Padhye@myob.com>
2018-11-30 08:18:23 +1300

    Trying to make the code look cleaner

--- lib/puppet_x/intechwifi/security_group_helper.rb
+++ lib/puppet_x/intechwifi/security_group_helper.rb
@@ -18,89 +18,58 @@ module PuppetX
     module IntechWIFI
         class SecurityGroupHelper
             def initialize(name, roles, services, label_format, coalesce_sgs)
-                @helper = coalesce_sgs ? SgPerRoleHelper.new(label_format) : SgPerServiceHelper.new(name, roles, services, label_format)
-            end
-
-            def generate_group_resources(status, region, tags, db_sgs, lb_sgs)
-                @helper.generate_group_resources(status, region, tags, db_sgs, lb_sgs)
-            end
-    
-            def generate_group_rules_resources
-                @helper.generate_group_rules_resources
-            end
-        end
-
-        class SgPerServiceHelper
-            def initialize(name, roles, services, label_format)
-                @label_format = (label_format.nil? || label_format.empty?) ? '%{vpc}_%{service}' : label_format
+                @helper = coalesce_sgs ? SgPerRoleHelper.new(label_format) : SgPerServiceHelper.new(label_format)
                 @name = name
                 @roles = roles
                 @services = services
             end
 
             def generate_group_resources(status, region, tags, db_sgs, lb_sgs)
-                (status == 'present' ? {
-                    #  Default security group for a vpc
+                (status == 'present' ? [{
+                    # Default security group for a vpc
                     @name => {
                         :ensure => status,
                         :region => region,
                         :vpc   => @name,
                         :tags => tags,
                     }
-                } : {}
-                ).merge(
-                    # Merge in the security group declarations for the services
-                    get_service_security_groups().map{|key, value|
-                    {
-                        key => {
-                            :ensure => status,
-                            :region => region,
-                            :vpc => @name,
-                            :tags => tags,
-                            :description => "Service security group"
-                        }
-                    }
-                    }.reduce({}){| hash, kv| hash.merge(kv)}
-                ).merge(
-                    # Merge in the security group declarations for the databases.
-                    db_sgs.map{|key, _val|
-                    {
-                        "#{@name}_#{key}" => {
-                            :ensure => status,
-                            :region => region,
-                            :vpc => @name,
-                            :tags => tags,
-                            :description => "database security group"
-                        }
-                    }
-                    }.reduce({}){| hash, kv| hash.merge(kv)}
-                ).merge(
-                    # Merge in the security group declarations for the load balancers.
-                    lb_sgs.map{|sg|
-                    {
-                        sg => {
-                            :ensure => status,
-                            :region => region,
-                            :vpc => @name,
-                            :tags => tags,
-                            :description => "load balancer security group"
-                        }
-                    }
-                    }.reduce({}){| hash, kv| hash.merge(kv)}
-                )
+                }] : []
+                ).concat(db_sgs.map{ |key, _val| generate_group_resource("#{@name}_#{key}", status, region, @name, tags, 'database security group') }) 
+                 .concat(lb_sgs.map{ |sg| generate_group_resource(sg, status, region, @name, tags, 'load balancer security group') })        
+                 .concat(@helper.generate_group_resources(@name, @roles, @services, status, region, tags))
+                 .reduce({}){ | hash, kv| hash.merge(kv) }
+            end
+
+
+            def generate_group_resource(resource_name, status, region, vpc, tags, description)
+                { resource_name => { :ensure => status, :region => region, :vpc => vpc, :tags => tags, :description => description } }
+            end
+
+            def generate_group_network_rules
+            end
+        end
+
+        class SgPerServiceHelper < SecurityGroupHelper
+            def initialize(label_format)
+                @label_format = (label_format.nil? || label_format.empty?) ? '%{vpc}_%{service}' : label_format
+            end
+
+            def generate_group_resources(name, roles, services, status, region, tags)
+                get_service_security_groups(name, roles, services)
+                    .map{ |sg, _val| generate_group_resource(sg, status, region, name, tags, 'Service security group') }
             end
 
             def generate_group_rules_resources
             end
 
-            def get_service_security_groups
-                @services.select{ |service, service_details| get_path_value(service_details, ['network', 'in'], []).length > 0 || get_path_value(service_details, ['network', 'out'], []).length > 0 }
+            def get_service_security_groups(name, roles, services)
+                services.select{ |service, service_details| get_path_value(service_details, ['network', 'in'], []).length > 0 || get_path_value(service_details, ['network', 'out'], []).length > 0 }
                         .map{ |service, service_details|
                                 {
-                                    get_service_security_group_name(service) => {
+                                    get_service_security_group_name(name, service) => {
                                         :service => service,
-                                        :in => get_path_value(service_details, ['network', 'in'], []).map{ |rule| transcode_rule(service, rule) }.flatten,
-                                        :out => get_path_value(service_details, ['network', 'out'], []).map{ |rule| transcode_rule(service, rule) }.flatten
+                                        :in => get_path_value(service_details, ['network', 'in'], []).map{ |rule| transcode_rule(name, roles, service, rule) }.flatten,
+                                        :out => get_path_value(service_details, ['network', 'out'], []).map{ |rule| transcode_rule(name, roles, service, rule) }.flatten
                                     }
                                 }
                             }
@@ -118,18 +87,18 @@ module PuppetX
                 end
             end
     
-            def get_service_security_group_name(service_name)
+            def get_service_security_group_name(name, service_name)
                 sprintf(@label_format, {
-                    :vpc => @name,
+                    :vpc => name,
                     :service => service_name,
-                    :VPC => @name.upcase,
+                    :VPC => name.upcase,
                     :SERVICE => service_name.upcase,
-                    :Vpc => @name.capitalize,
+                    :Vpc => name.capitalize,
                     :Service => service_name.capitalize,
                 })
             end
     
-            def transcode_rule(service, env_format)
+            def transcode_rule(name, roles, service, env_format)
                 segments = env_format.split('|')
     
                 case segments[2]
@@ -139,21 +108,18 @@ module PuppetX
                     case segments[3]
                     when 'elb'
                         # This is the fun one!
-                        location_ident = @roles.select{|role_name, role_data|
+                        location_ident = roles.select{|role_name, role_data|
                         role_data['services'].include?(service)
                         }.map{|role_name, role_data|
-                        "#{@name}_#{role_name}_elb"
+                        "#{name}_#{role_name}_elb"
                         }
-                    
-    
                     end
-    
                 when 'service'
                     location_type = 'sg'
-                    location_ident = [get_service_security_group_name(segments[3])]
+                    location_ident = [get_service_security_group_name(name, segments[3])]
                 when 'rds'
                     location_type = 'sg'
-                    location_ident = ["#{@name}_#{segments[3]}"]
+                    location_ident = ["#{name}_#{segments[3]}"]
                 else
                     location_type = segments[2]
                     location_ident = [segments[3]]
@@ -163,7 +129,7 @@ module PuppetX
             end
         end
       
-        class SgPerRoleHelper
+        class SgPerRoleHelper < SecurityGroupHelper
             def initialize(sg_label_format)
                 @label_format = (sg_label_format.nil? || sg_label_format.empty?) ? '%{vpc}_%{role}' : sg_label_format
             end
