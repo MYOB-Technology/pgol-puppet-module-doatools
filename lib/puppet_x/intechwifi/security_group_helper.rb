15299d763fe75c4b8534d0b71179793db1e25461
Shantanu Padhye <Shantanu.Padhye@myob.com>
2018-11-30 14:34:51 +1300

    More clean

--- lib/puppet_x/intechwifi/security_group_helper.rb
+++ lib/puppet_x/intechwifi/security_group_helper.rb
@@ -44,9 +44,6 @@ module PuppetX
             def generate_group_resource(resource_name, status, region, vpc, tags, description)
                 { resource_name => { :ensure => status, :region => region, :vpc => vpc, :tags => tags, :description => description } }
             end
-
-            def generate_group_network_rules
-            end
         end
 
         class SgPerServiceHelper < SecurityGroupHelper
@@ -59,21 +56,10 @@ module PuppetX
                     .map{ |sg, _val| generate_group_resource(sg, status, region, name, tags, 'Service security group') }
             end
 
-            def generate_group_rules_resources
-            end
-
             def get_service_security_groups(name, roles, services)
                 services.select{ |service, service_details| get_path_value(service_details, ['network', 'in'], []).length > 0 || get_path_value(service_details, ['network', 'out'], []).length > 0 }
-                        .map{ |service, service_details|
-                                {
-                                    get_service_security_group_name(name, service) => {
-                                        :service => service,
-                                        :in => get_path_value(service_details, ['network', 'in'], []).map{ |rule| transcode_rule(name, roles, service, rule) }.flatten,
-                                        :out => get_path_value(service_details, ['network', 'out'], []).map{ |rule| transcode_rule(name, roles, service, rule) }.flatten
-                                    }
-                                }
-                            }
-                        .reduce({}){ |hash, kv| hash.merge(kv) }
+                        .keys
+                        .map{ |service| get_security_group_name(name, service) }
             end
     
             def get_path_value(data, path, nodata)
@@ -87,7 +73,7 @@ module PuppetX
                 end
             end
     
-            def get_service_security_group_name(name, service_name)
+            def get_security_group_name(name, service_name)
                 sprintf(@label_format, {
                     :vpc => name,
                     :service => service_name,
@@ -97,36 +83,6 @@ module PuppetX
                     :Service => service_name.capitalize,
                 })
             end
-    
-            def transcode_rule(name, roles, service, env_format)
-                segments = env_format.split('|')
-    
-                case segments[2]
-                when 'rss'
-                    location_type = 'sg'
-    
-                    case segments[3]
-                    when 'elb'
-                        # This is the fun one!
-                        location_ident = roles.select{|role_name, role_data|
-                        role_data['services'].include?(service)
-                        }.map{|role_name, role_data|
-                        "#{name}_#{role_name}_elb"
-                        }
-                    end
-                when 'service'
-                    location_type = 'sg'
-                    location_ident = [get_service_security_group_name(name, segments[3])]
-                when 'rds'
-                    location_type = 'sg'
-                    location_ident = ["#{name}_#{segments[3]}"]
-                else
-                    location_type = segments[2]
-                    location_ident = [segments[3]]
-                end
-    
-                location_ident.map{|loc_ident|  "#{segments[0]}|#{segments[1]}|#{location_type}|#{loc_ident}" }
-            end
         end
       
         class SgPerRoleHelper < SecurityGroupHelper
@@ -135,9 +91,12 @@ module PuppetX
             end
 
             def generate_group_resources
+                get_role_security_groups(name, roles, services)
+                    .map{ |sg, _val| generate_group_resource(sg, status, region, name, tags, 'Service security group') }
             end
 
-            def generate_group_rules_resources
+            def get_role_security_groups(name, roles, services)
+                
             end
         end
     end
