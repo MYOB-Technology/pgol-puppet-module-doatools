4741a3dff4f76ca6293cfcbd9111663dbcab78f5
Shantanu Padhye <Shantanu.Padhye@myob.com>
2019-03-19 09:34:03 +1300

    Created simple lambda puppet type

---
+++ lib/puppet/type/lambda.rb
@@ -0,0 +1,45 @@
+#  Copyright (C) 2017 IntechnologyWIFI / Michael Shaw
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'puppet_x/intechwifi/logical'
+require 'puppet_x/intechwifi/constants'
+
+Puppet::Type.newtype(:lambda) do
+  ensurable
+
+  newparam(:name, :namevar => true) do
+  end
+
+  newparam(:region) do
+    defaultto 'ap-southeast-2'
+  end
+
+  newproperty(:runtime) do
+  end
+
+  newproperty(:role) do
+  end
+
+  newproperty(:handler) do
+  end
+
+  newproperty(:s3_bucket) do
+    desc "simple"
+  end
+
+  newproperty(:s3_key) do
+      desc "simple"
+  end
+end
