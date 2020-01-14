6877f72124a21a6510a1850d0f1303f97929b32b
Michael <micptolshaw@gmail.com>
2020-01-15 03:34:45 +1300

    removed diagnostic statement

--- lib/puppet/provider/launch_configuration/awscli.rb
+++ lib/puppet/provider/launch_configuration/awscli.rb
@@ -115,7 +115,6 @@ Puppet::Type.type(:launch_configuration).provide(:awscli) do
 
     #@property_hash[:image_disks] = PuppetX::IntechWIFI::EBS_Volumes.get_image_disks_from_block_device_mapping(block_device_mapping, ami_block_device_mapping)
     #@property_hash[:extra_disks] = PuppetX::IntechWIFI::EBS_Volumes.get_extra_disks_from_block_device_mapping(block_device_mapping, ami_block_device_mapping)
-    # print "launch_config = #{launch_config}\n"
     true
 
   rescue PuppetX::IntechWIFI::Exceptions::NotFoundError => e
