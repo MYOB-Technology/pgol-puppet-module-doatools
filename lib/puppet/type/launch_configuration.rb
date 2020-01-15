635cfc39c56217679c2839e11fc65134870e4147
Michael <micptolshaw@gmail.com>
2020-01-15 16:49:50 +1300

    checking if the disk images need changing

--- lib/puppet/type/launch_configuration.rb
+++ lib/puppet/type/launch_configuration.rb
@@ -105,8 +105,19 @@ Puppet::Type.newtype(:launch_configuration) do
       puts "should=#{@should}"
       puts "is=#{is}"
 
-      # make sure that all hash keys defined are set on the resource, and are the same value
-      #@should.keys.all?{|v| @is.has_key v} and @should.keys.all?{|v| is[v] == @should[v]}
+      # do we have a disk defined
+      result = ( @should.length == 0 and is.keys.length == 0 ) or
+      @should.all{|s|
+          # all top level keys are devices. These lists should contain the same keys if we are in sync
+          s.keys.all{|device| is.has_key device} and
+            is.keys.all{|device| s.has_key device} and
+            s.keys.all{|device|
+              # then we need to check each device in turn and make sure that all should keys and value are identical in is.
+              s[device].keys.all{|param|
+                is[device]._has_key param and is[device][param] == s[device][param]
+              }
+            }
+      puts "result=#{result}"
     end
 
   end
